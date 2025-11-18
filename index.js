2f75a9d8711a11f78d26efcb3dd07ceaacbc8bf6
Mateusz Lisowski <84307013+lisiaczeq@users.noreply.github.com>
2025-11-18 08:55:31 +0000

    Com4_2.1be

--- index.js
+++ index.js
@@ -3,17 +3,57 @@
 const express = require('express');
 const app = express();
 
-// define endpoint for exercise 1 here
-app.get('/math/circle/:r', (req, res) => {
-//TODO1  
-  res.json(result);
+let categories = ['funnyJoke', 'lameJoke'];
+
+let funnyJoke = [
+  {
+    'joke': 'Dlaczego komputer poszedł do lekarza?',
+    'response': 'Bo złapał wirusa!'
+  },
+  {
+    'joke': 'Dlaczego komputer nie może być głodny?',
+    'response': 'Bo ma pełen dysk!'
+  },
+  {
+    'joke': 'Co mówi jeden bit do drugiego?',
+    'response': '„Trzymaj się, zaraz się przestawiamy!"'
+  }
+];
+
+let lameJoke = [
+  {
+    'joke': 'Dlaczego programiści preferują noc?',
+    'response': 'Bo w nocy jest mniej bugów do łapania!'
+  },
+  {
+    'joke': 'Jak nazywa się bardzo szybki programista?',
+    'response': 'Błyskawiczny kompilator!'
+  }
+];
+
+app.get('/jokebook/categories', (req, res) => {
+  res.json(categories);
 });
 
-//TODO2
-
-
-//TODO3
-
+app.get('/jokebook/joke/:category', (req, res) => {
+  const category = req.params.category;
+  
+  if (!categories.includes(category)) {
+    return res.json({ 'error': `no jokes for category ${category}` });
+  }
+  
+  let jokes;
+  if (category === 'funnyJoke') {
+    jokes = funnyJoke;
+  } else if (category === 'lameJoke') {
+    jokes = lameJoke;
+  }
+  
+  const randomIndex = Math.floor(Math.random() * jokes.length);
+  const randomJoke = jokes[randomIndex];
+  
+  res.json(randomJoke);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
